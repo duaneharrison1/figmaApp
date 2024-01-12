777a14c51bbc73898b3ea95f29b151c07b8d0180
francis gonzales <fgonzales.private@icloud.com>
2024-01-12 09:05:50 +0800

    Update DynamicPage2.js

--- src/pages/DynamicPage2.js
+++ src/pages/DynamicPage2.js
@@ -35,7 +35,7 @@ function DynamicPage2() {
               if (subData.items[0].plan.id == process.env.REACT_APP_MONTHLY) {
                 mainDoc.subcollectionUrlData.forEach((url, urlIndex) => {
                   setUrlData(url)
-                  console.log("UrlData " + urlData)
+                  console.log("UrlData " + url)
 
                   const modifiedCustomDomain = url.customDomain.replace(/^(https?:\/\/)?(www\.)?/, '');
                   const modifiedDomain = domain.replace(/^(https?:\/\/)?(www\.)?/, '');
