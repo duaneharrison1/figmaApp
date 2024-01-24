5416d072de0b7c6adaf72c351d130ea5d45df672
francis gonzales <fgonzales.private@icloud.com>
2024-01-24 19:44:10 +0800

    update forms

--- src/pages/EditForm/EditForm.js
+++ src/pages/EditForm/EditForm.js
@@ -11,6 +11,10 @@ import Navbar from '../../components/NavBar/Navbar';
 import ButtonClear from '../../components/ButtonClear/ButtonClear';
 import PaymentSelectionModal from '../../components/PaymentSelection/PaymentSelection';
 import AlertErrorModal from '../../components/AlertErrorModal/AlertErrorModal';
+import insOne from './../../assets/images/ins-1.png';
+import insTwo from './../../assets/images/ins-2.png';
+import insThree from './../../assets/images/ins-3.png';
+import insFour from './../../assets/images/ins4.png';
 export default function EditForm() {
     const navigate = useNavigate();
     const location = useLocation();
@@ -160,8 +164,8 @@ export default function EditForm() {
 
                     <div className='form-container'>
                         <div className='row first-div'>
+                            <h1 className='form-title'>{title}</h1>
                             <div className='col-md-6'>
-                                <h1 className='form-title'>General</h1>
                                 <h2 className='form-sub-header'>Title</h2>
                                 <input
                                     className='input'
@@ -224,20 +228,52 @@ export default function EditForm() {
                             </div>
                         </div>
 
-
                         <div className='second-div'>
-                            <h1 className='form-title'>Figma prototype links</h1>
-                            {/* <p className='note'> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="#424242" stroke-width="2" stroke-linecap="round" strokeLinejoin="round" />
-                                </svg> You should hide hide hotspot hints by selecting the Options menu in the prototype of Figma for a better experience</p> */}
-                            <p className='note'> Paste in the links of your desktop and mobile prototypes below to have them create your Figmafolio site. By adding separate desktop and mobile links, all viewers can easily preview your work on any device. We'll detect the device and load the appropriate prototype</p>
-                            <p className='note'> Here are some tips: </p>
-                            <ul className='instruction'>
-                                <li className='note'>Paste in the link to a prototype or specific prototype flow, not the full Figma file</li>
-                                <li className='note'>If you do not have a separate mobile prototype, you can paste in the desktop prototype link to show the desktop view on mobile.</li>
-                                <li className='note'>For best results, match prototype’s background colour to your sites background colour in Figma in prototype settings.</li>
-                            </ul>
+                            <div className='row'>
+                                <div className="col-sm-3">
+
+                                    <div className='div-instruction-img'>
+                                        <img src={insOne} className='instruction-img' />
+                                    </div>
+                                    <h1 className='instruction-title'>Design in Figma</h1>
+                                    <p className='instructions'>1. Open Figma and design each page of your portfolio or website</p>
+                                    <p className='instructions'> Note: It is recommended to create separate designs for both desktop and mobile pages.</p>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className='div-instruction-img'>
+                                        <img src={insTwo} className='instruction-img' />
+                                    </div>
+                                    <h1 className='instruction-title'>Prototype in Figma</h1>
+                                    <p className='instructions'>1. Switch to "Prototype" mode in the right sidebar.</p>
+                                    <p className='instructions'>2. Draw connections between pages to define the navigation of your site.</p>
+                                    <p className='instructions'>3. Ideally create two flows: one for desktop and one for mobile. </p>
+                                    <p className='instructions'> Note: You can add animations to connections to enhance any transitions.</p>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className='div-instruction-img'>
+                                        <img src={insThree} className='instruction-img' />
+                                    </div>
+                                    <h1 className='instruction-title'>Set flow starting points</h1>
+                                    <p className='instructions'>1. For your desktop prototype, select the frame users will see. Typically the homepage.</p>
+                                    <p className='instructions'>2. In the right sidebar under Prototype, click the ‘+’ next to ‘Flow starting point’ to set the flow starting point. </p>
+                                    <p className='instructions'>3. Repeat for your mobile prototype if you have one. </p>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className='div-instruction-img'>
+                                        <img src={insFour} className='instruction-img' />
+                                    </div>
+
+                                    <h1 className='instruction-title'>Input your prototype linkss</h1>
+                                    <p className='instructions'>1. Click the canvas in Figma to get  file properties in the right sidebar.</p>
+                                    <p className='instructions'>2. Select ‘Prototype’. Under ‘Flows’ .hover over each flow and "Copy link" to get the prototype links. </p>
+                                    <p className='instructions'>3. Paste in the corresponding prototype links below. </p>
+                                    <p className='instructions'>4. Click "Preview" to view and publish your portfolio/site. </p>
+                                    <p className='instructions'>Note: If you only provide one prototype link, Figmafolio will show that flow on both desktop and mobile.</p>
+                                </div>
+                            </div>
+
                         </div>
+
 
                         <div className="fifth-div">
                             <div className="row">
