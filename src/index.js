65f118159ecc8d4387463f0d7a44284613a7bf1a
francis gonzales <fgonzales.private@icloud.com>
2024-02-01 07:41:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
 import { Analytics } from '@vercel/analytics/react';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
