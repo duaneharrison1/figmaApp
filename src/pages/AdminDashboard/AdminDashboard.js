92c40d5bf242e624fb05a912b862a482c4a5aced
francis gonzales <fgonzales.private@icloud.com>
2024-02-13 08:22:14 +0800

    Update AdminDashboard.js

--- src/pages/AdminDashboard/AdminDashboard.js
+++ src/pages/AdminDashboard/AdminDashboard.js
@@ -7,97 +7,161 @@ import { collection, getDocs, collectionGroup, Timestamp, query, orderBy } from 
 import './AdminDashboard.css'
 import firebase from '../../firebase';
 import { compareAsc, format } from "date-fns";
+import { Table, Button, ButtonGroup } from "react-bootstrap";
+import ButtonColored from '../../components/ButtonColored/ButtonColored';
 function AdminDashboard() {
+    const dbFirestore = firebase.firestore();
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
     const [data, setData] = useState([]);
     const [profile, setProfile] = useState();
     const [searchTerm, setSearchTerm] = useState('');
+    const [page, setPage] = useState(1);
+    const [isAdmin, setIsAdmin] = useState();
     // State for handling sorting
     const [sortOrder, setSortOrder] = useState('asc');
     useEffect(() => {
+        console.log("gggg went here")
         const unsubscribe = auth.onAuthStateChanged((user) => {
             setUser(user);
         });
-        return () => unsubscribe();
-    }, []);
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            if (user) {
-                try {
-                    await getDocs(collection(db, "user", user.uid, "profile"))
-                        .then((querySnapshot) => {
-                            const userProfile = querySnapshot.docs
-                                .map((doc) => ({ ...doc.data(), id: doc.id }));
-                            setProfile(userProfile);
-                        })
-                    if (profile[0].isAdmin == "true") {
-                        try {
-                            const collectionRef = collectionGroup(db, "url");
-                            const snapshot = await getDocs(query(collectionRef, orderBy('createdAt', 'desc')));
-                            // const snapshot = await getDocs(collectionRef);
-                            const fetchedData = snapshot.docs.map(doc => doc.data());
-                            setData(fetchedData);
-                        } catch (error) {
-                            console.error('Error fetching data:', error);
+        if (user) {
+            try {
+                getDocs(collection(db, "user", user.uid, "profile"))
+                    .then((querySnapshot) => {
+                        const userProfile = querySnapshot.docs
+                            .map((doc) => ({ ...doc.data(), id: doc.id }));
+                        setProfile(userProfile);
+                        if (userProfile[0].isAdmin == "true") {
+                            fetchData()
+                        } else {
+                            navigate("/");
                         }
-                    } else {
-                        navigate("/");
-                    }
-
-                } catch (error) {
-                    console.error('Error fetching data:', error);
-                }
-
-
+                    })
+            } catch (error) {
+                console.error('Error fetching data:', error);
             }
-            else {
-            }
+        }
+        else {
+        }
+        return () => unsubscribe();
+    }, [user]);
+
+
+    const fetchData = async () => {
+        try {
+            dbFirestore.collectionGroup('url').orderBy('createdAt', 'desc').onSnapshot(function (querySnapshot) {
+                var items = [];
+                querySnapshot.forEach(function (doc) {
+                    items.push({ key: doc.id, ...doc.data() });
+                });
+                console.log('first item ', items[0])
+                setData(items);
+            })
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    const showNext = ({ item }) => {
+        if (data.length === 0) {
+            alert("Thats all we have for now !")
+        } else {
+            const fetchNextData = async () => {
+                await dbFirestore.collectionGroup('url')
+                    .orderBy('createdAt', 'desc')
+                    .limit(10)
+                    .startAfter(item.createdAt)
+                    .onSnapshot(function (querySnapshot) {
+                        const items = [];
+                        querySnapshot.forEach(function (doc) {
+                            items.push({ key: doc.id, ...doc.data() });
+                        });
+                        setData(items);
+                        setPage(page + 1)
+                    })
+            };
+            fetchNextData();
+        }
+    };
+
+    const showPrevious = ({ item }) => {
+        const fetchPreviousData = async () => {
+            await dbFirestore.collectionGroup('url')
+                .orderBy('createdAt', 'desc')
+                .endBefore(item.createdAt)
+                .limitToLast(10)
+                .onSnapshot(function (querySnapshot) {
+                    const items = [];
+                    querySnapshot.forEach(function (doc) {
+                        items.push({ key: doc.id, ...doc.data() });
+                    });
+                    setData(items);
+                    setPage(page - 1)
+                })
         };
-        fetchData();
-    }, [user, profile]);
-
-
-
+        fetchPreviousData();
+    };
 
     return (
-        <table className='table table-striped'>
-
-            <thead>
-                <tr>
-
-                    <th>Title</th>
-                    <th>Generated Url</th>
-                    <th>Custom Domain</th>
-                    <th>Draft</th>
-
-                    <th>Created At</th>
-                    <th>Update At</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item, index) => (
-                    <tr key={index}>
-                        <td>{item.title}</td>
-                        <td>  <a href={`${item.generatedUrl}`} target="_blank" rel="noopener noreferrer">
-                            {`www.figmafolio/${item.generatedUrl}`}
-                        </a></td>
+        <div className='folio-data-container'>
 
 
-                        <td>{item.customDomain}</td>
-                        <td>{item.isDraft}</td>
-                        <td>{new Date(item?.createdAt?.seconds * 1000).toLocaleDateString("en-US")}</td>
-                        <td>{new Date(item?.updatedAt?.seconds * 1000).toLocaleDateString("en-US")}</td>
+            <table className='table table-striped'>
+
+                <thead>
+                    <tr>
+
+                        <th>Title</th>
+                        <th>Generated Url</th>
+                        <th>Custom Domain</th>
+                        <th>Draft</th>
+
+                        <th>Created At</th>
+                        <th>Update At</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.title}</td>
+                            <td>  <a href={`${item.generatedUrl}`} target="_blank" rel="noopener noreferrer">
+                                {`www.figmafolio/${item.generatedUrl}`}
+                            </a></td>
 
+
+                            <td>{item.customDomain}</td>
+                            <td>{item.isDraft}</td>
+                            <td>{new Date(item?.createdAt?.seconds * 1000).toLocaleDateString("en-US")}</td>
+                            <td>{new Date(item?.updatedAt?.seconds * 1000).toLocaleDateString("en-US")}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+
+            {/* <ButtonGroup size="lg" className="mb-2">
+                {
+                    //show previous button only when we have items
+                    page === 1 ? '' :
+                        <Button onClick={() => showPrevious({ item: data[0] })}>Previous</Button>
+                }
+
+                {
+                    //show next button only when we have items
+                    data.length < 10 ? '' :
+                        <Button onClick={() => showNext({ item: data[data.length - 1] })}>Next</Button>
+                }
+            </ButtonGroup> */}
+        </div>
     );
 }
 
 export default AdminDashboard;
+
+
+
+
+
 
 
