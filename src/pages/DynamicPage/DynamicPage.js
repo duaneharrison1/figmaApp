daca0960e0a82d49c5b338f86cf707a4eecfa55c
francis gonzales <fgonzales.private@icloud.com>
2024-03-25 09:55:58 +0800

    update dynamic page to detect subscription

--- src/pages/DynamicPage/DynamicPage.js
+++ src/pages/DynamicPage/DynamicPage.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './DynamicPage.css';
+import firebase from '../../firebase';
 function DynamicPage({ url }) {
-
+  const dbFirestore = firebase.firestore();
   document.title = url.title;
+
   const [isMobile, setIsMobile] = useState(false);
   const [mobile, setMobile] = useState("");
   const [desktop, setDesktop] = useState("");
+  const [activeSubscriber, setActiveSubscriber] = useState("");
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
-    handleResize();
 
-    if (url.urls.figmaMobileUrl == "") {
+
+    dbFirestore.collection('user').doc(url.userId).collection("subscriptions").orderBy('created', 'desc').limit(1).get().then(snapshot => {
+      if (snapshot.size === 0) {
+        setActiveSubscriber("false")
+      } else {
+        snapshot.forEach(subscription => {
+          if (subscription.data().status == "active") {
+            setActiveSubscriber("true")
+          }
+        }
+        )
+      }
+    })
+
+    if (url.urls.figmaMobileUrl === "") {
       setMobile(url.urls.figmaDesktopUrl)
     } else {
       setMobile(url.urls.figmaMobileUrl)
     }
 
-    if (url.urls.figmaDesktopUrl == "") {
+    if (url.urls.figmaDesktopUrl === "") {
       setDesktop(url.urls.figmaMobileUrl)
     } else {
       setDesktop(url.urls.figmaDesktopUrl)
@@ -33,9 +49,10 @@ function DynamicPage({ url }) {
   }, []);
   return (
     <>
-      <div className="text-overlay">
-        <p className='made-with'>Made with <span className="made-with-figmaolio">Figmafolio</span></p>
-      </div>
+      {activeSubscriber == "true" ? (<div></div>) :
+        (<div className="text-overlay">
+          <p className='made-with'>Made with <span className="made-with-figmaolio">Figmafolio</span></p>
+        </div>)}
       <iframe
         src={isMobile ? mobile : desktop}
         allowFullScreen
