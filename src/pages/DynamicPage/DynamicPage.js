51c0a7c4e291fc60a63cee40d583421843249d9a
francis gonzales <fgonzales.private@icloud.com>
2024-03-31 23:02:36 +0800

    remove scrollbar in dynamic view

--- src/pages/DynamicPage/DynamicPage.js
+++ src/pages/DynamicPage/DynamicPage.js
@@ -66,7 +66,7 @@ function DynamicPage({ url }) {
         allowFullScreen
         referrerpolicy="no-referrer"
         style={{ width: '100%', height: '100vh' }}
-        className='dynamic_view_figma_view'>
+        className='dynamicpage_view_figma_view'>
       </iframe>
     </>
 
